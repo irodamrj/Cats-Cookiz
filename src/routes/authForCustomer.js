4e7719da750dc7040c1651f96e60e503ce561c47
irodaMR <irodamirjalolova@eskisehir.edu.tr>
2023-07-27 14:41:47 +0300

    facebook and google auth updated

--- src/routes/authForCustomer.js
+++ src/routes/authForCustomer.js
@@ -1,33 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const { attachCookiesToResponse } = require('../utils/jwt');
+const { createUserToken } = require('../utils/createToken');
+const { StatusCodes } = require('http-status-codes');
 
 const checkCookie = require('../middleware/checkCookie');
 const { customerAuth } = require('../middleware/authorization');
-const {
-  google,
-  googleCallbackFunc,
-  facebook,
-  facebookCb,
-  signup,
-  login,
-  logout,
-} = require('../controllers/authForCustomer');
+const { signup, login, logout } = require('../controllers/authForCustomer');
 
-router.get('/google', google);
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email', 'openid'],
+  })
+);
 router.get(
   '/google/callback',
   passport.authenticate('google', {
     session: false,
     failureRedirect: '/',
   }),
-  googleCallbackFunc
+  async (req, res) => {
+    const payload = createUserToken(req.user);
+    console.log(payload);
+    attachCookiesToResponse(res, payload);
+    return res.status(StatusCodes.OK).json(req.user);
+  }
 );
-router.get('/facebook', facebook);
+router.get(
+  '/facebook',
+  passport.authenticate('facebook', { scope: ['public_profile', 'email'] })
+);
 router.get(
   '/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
-  facebookCb
+  function (req, res) {
+    const payload = createUserToken(req.user);
+    attachCookiesToResponse(res, payload);
+    return res.status(StatusCodes.OK).json(req.user);
+  }
 );
 router.post('/signup', checkCookie, signup);
 router.post('/login', checkCookie, login);
