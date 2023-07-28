269812a2e7a311b6c7235929d220aa2edbb6d021
irodaMR <irodamirjalolova@eskisehir.edu.tr>
2023-07-28 21:21:20 +0300

    var is changed to const

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,15 +2,15 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 sendEmail = async function (emailAccount, message) {
-  var to = 'gogohatiko@gmail.com';
-  var smtpTransport = nodemailer.createTransport({
+  const to = 'gogohatiko@gmail.com';
+  const smtpTransport = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
       user: process.env.NODEMAILER_MAIL,
       pass: process.env.NODEMAILER_PASSWORD,
     },
   });
-  var mailOptions = {
+  const mailOptions = {
     from: '"Recoded Capstone" <capstonecats@gmail.com>',
     to: to,
     subject: 'Account creation',
